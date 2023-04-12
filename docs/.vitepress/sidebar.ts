1fa8ef37c22eddc27b8037e2d30d03cef445cc72
HangSkr <9903738+hangskr@user.noreply.gitee.com>
2023-04-12 09:58:34 +0800

    node

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -1,8 +1,12 @@
 import {DefaultTheme} from "vitepress";
 // @ts-ignore
 import learnSidebar from '../learn/sidebar'
+import sourceListSidebar from '../source-list/sidebar'
+
 
 const sidebar: DefaultTheme.Sidebar = {
-    '/learn':learnSidebar
+    '/learn':learnSidebar,
+    '/source-list/': sourceListSidebar,
+
 }
 export default sidebar
